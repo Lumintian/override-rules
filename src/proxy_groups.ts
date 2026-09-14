258f66b799523dda365975384df928ecec9fae6a
𝓛𝓾𝓶𝓲𝓷𝓽𝓲𝓪𝓷 <github@lumintian.com>
2026-09-14 10:48:18 +0800

    chore: update self-hosted icon URLs

--- src/proxy_groups.ts
+++ src/proxy_groups.ts
@@ -140,7 +140,7 @@ export function buildProxyGroups({
         },
         {
             name: PROXY_GROUPS.MICROSOFT,
-            icon: `${CDN_URL}/gh/powerfullz/override-rules@master/icons/Microsoft_Copilot.png`,
+            icon: `${CDN_URL}/gh/Lumintian/override-rules@main/icons/Microsoft_Copilot.png`,
             type: "select",
             proxies: defaultProxies,
         },
@@ -202,7 +202,7 @@ export function buildProxyGroups({
         },
         {
             name: PROXY_GROUPS.TELEGRAM,
-            icon: `${CDN_URL}/gh/powerfullz/override-rules@master/icons/Telegram.png`,
+            icon: `${CDN_URL}/gh/Lumintian/override-rules@main/icons/Telegram.png`,
             type: "select",
             proxies: defaultProxies,
         },
@@ -221,7 +221,7 @@ export function buildProxyGroups({
         },
         {
             name: PROXY_GROUPS.TRUTH_SOCIAL,
-            icon: `${CDN_URL}/gh/powerfullz/override-rules@master/icons/Truth_Social.png`,
+            icon: `${CDN_URL}/gh/Lumintian/override-rules@main/icons/Truth_Social.png`,
             type: "select",
             proxies: hasUS
                 ? [`美国节点`, PROXY_GROUPS.SELECT, PROXY_GROUPS.MANUAL]
@@ -229,26 +229,26 @@ export function buildProxyGroups({
         },
         {
             name: PROXY_GROUPS.EHENTAI,
-            icon: `${CDN_URL}/gh/powerfullz/override-rules@master/icons/Ehentai.png`,
+            icon: `${CDN_URL}/gh/Lumintian/override-rules@main/icons/Ehentai.png`,
             type: "select",
             proxies: defaultProxies,
         },
         {
             name: PROXY_GROUPS.PIKPAK,
-            icon: `${CDN_URL}/gh/powerfullz/override-rules@master/icons/PikPak.png`,
+            icon: `${CDN_URL}/gh/Lumintian/override-rules@main/icons/PikPak.png`,
             type: "select",
             proxies: defaultProxies,
         },
         {
             name: PROXY_GROUPS.SOGOU_INPUT,
-            icon: `${CDN_URL}/gh/powerfullz/override-rules@master/icons/Sougou.png`,
+            icon: `${CDN_URL}/gh/Lumintian/override-rules@main/icons/Sougou.png`,
             type: "select",
             proxies: ["DIRECT", "REJECT"],
         },
         hasTailscale
             ? {
                   name: PROXY_GROUPS.TAILSCALE,
-                  icon: `${CDN_URL}/gh/powerfullz/override-rules@master/icons/Tailscale.png`,
+                  icon: `${CDN_URL}/gh/Lumintian/override-rules@main/icons/Tailscale.png`,
                   type: "select",
                   proxies: tailscaleNodes.map((node) => node.name).filter(isNotNull),
               }
